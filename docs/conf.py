46fc157a23ddc32afafd7fd7baabe18ac0a0ba13
Saad Rahim <saad.rahim@amd.com>
2023-06-28 18:05:54 -0600

    external project

--- docs/conf.py
+++ docs/conf.py
@@ -9,5 +9,7 @@ from rocm_docs import ROCmDocs
 docs_core = ROCmDocs("HIPCC Documentation")
 docs_core.setup()
 
+external_projects_current_project = "hip"
+
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
