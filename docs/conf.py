6fa73a27b704bc88e0490351b3f945fac45a367f
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-05-03 14:59:19 -0600

    Add archive page

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# configurations for PDF output by Read the Docs
+project = "HIPCC Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+version = "archive"
+
+exclude_patterns = ['temp']
+
+external_toc_path = "./sphinx/_toc.yml"
+
+extensions = ["rocm_docs"]
+
+external_projects_current_project = "hipcc"
+
+html_theme = "rocm_docs_theme"
+html_theme_options = {"flavor": "rocm-docs-home"}
+
+html_title = "HIPCC Documentation"
+
+html_theme_options = {
+    "link_main_doc": False
+}
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# HIPCC
+
+This is a deprecated, read-only mirror. It is kept up-to-date to facilitate transition to its new home in the llvm-project monorepo, hosted under the amd/ subdirectory at: https://github.com/ROCm/llvm-project.
